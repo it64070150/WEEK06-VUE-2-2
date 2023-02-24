d06642db2c7ba53c42502ef1a04673ea80e6671a
bundittsp <bundittsp@gmail.com>
2023-02-24 08:58:00 +0700

    Remove tmpMac.js

--- ex2/tmpMac.js
+++
@@ -1,8 +0,0 @@
-import tambons from './data/tambons.json' assert {type: 'json'}
-
-let ids = amphures.map(val=>val.id)
-
-let tambonjud = tambons.filter(tambon=>ids.includes( tambon.amphure_id))
-
-
-console.log(JSON.stringify(tambonjud, null, 4))
